1761451e5514d1def97ade3e55a86e382f85b9b0
Andy <andy999coc@gmail.com>
2019-10-29 22:23:19 -0400

    added hinge loss

---
+++ Big GAN/losses.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+
+
+# Hinge loss from SAGAN
+def loss_hinge_dis(d_real, d_fake):
+    d_loss_real = torch.mean(F.relu(1.0 - d_real))
+    d_loss_fake = torch.mean(F.relu(1.0 + d_fake))
+    return d_loss_real, d_loss_fake
+
+
+def loss_hinge_gen(d_fake):
+    g_loss_fake = -torch.mean(d_fake)
+    return g_loss_fake
